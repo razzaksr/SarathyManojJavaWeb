03553de86efe108265d1405084299a99005f03cd
razzaksr <razzaksr@gmail.com>
2021-08-30 13:47:42 +0530

    first commit

---
+++ ManojSarathyJava/src/main/java/java/sarathy/manoj/ManojSarathyJava/App.java
@@ -0,0 +1,13 @@
+package java.sarathy.manoj.ManojSarathyJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ManojSarathyJava/src/test/java/java/sarathy/manoj/ManojSarathyJava/AppTest.java
@@ -0,0 +1,20 @@
+package java.sarathy.manoj.ManojSarathyJava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
