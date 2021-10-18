7a57e87046a039893a4a7de6234e5feffa856130
razzaksr <razzaksr@gmail.com>
2021-10-18 11:50:51 +0530

    Array started

--- ManojSarathyJava/src/main/java/sarathy/manoj/ManojSarathyJava/Patterns.java
+++ ManojSarathyJava/src/main/java/sarathy/manoj/ManojSarathyJava/Patterns.java
@@ -25,6 +25,18 @@ public class Patterns
 			System.out.println();
 		}
 		
+		
+		//left lower floyd
+		num='A';
+		for(int row=5;row>0;row--)
+		{
+			for(int col=1;col<=row;col++,num++)
+			{
+				System.out.print(num);
+			}
+			System.out.println();
+		}
+		
 		//right upper floyd
 		num='A';
 		for(int row=1;row<=5;row++)
@@ -40,5 +52,82 @@ public class Patterns
 			System.out.println();
 		}
 		
+		// right lower floyd
+		num='A';
+		for(int row=5;row>0;row--)
+		{
+			for(int space=5-1;space>=row;space--)
+			{
+				System.out.print(" ");
+			}
+			for(int col=1;col<=row;col++,num++)
+			{
+				System.out.print(num);
+			}
+			System.out.println();
+		}
+		
+		//pascal upper
+		num='A';
+		for(int row=1;row<=5;row++)
+		{
+			for(int space=5-1;space>=row;space--)
+			{
+				System.out.print(" ");
+			}
+			for(int col=1;col<=row;col++,num++)
+			{
+				System.out.print(num+" ");
+			}
+			System.out.println();
+		}
+		
+		//pascal lower
+		num='A';
+		for(int row=5;row>0;row--)
+		{
+			for(int space=5-1;space>=row;space--)
+			{
+				System.out.print(" ");
+			}
+			for(int col=1;col<=row;col++,num++)
+			{
+				System.out.print(num+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		//pyramid upper
+		num='A';int limit=1;
+		for(int row=1;row<=5;row++)
+		{
+			for(int space=5-1;space>=row;space--)
+			{
+				System.out.print(" ");
+			}
+			for(int col=1;col<=limit;col++,num++)
+			{
+				System.out.print(num);
+			}
+			System.out.println();
+			limit+=2;
+		}
+		
+		//pyramid lower
+		num='A';limit=(5*2)-1;
+		for(int row=5;row>0;row--)
+		{
+			for(int space=5-1;space>=row;space--)
+			{
+				System.out.print(" ");
+			}
+			for(int col=1;col<=limit;col++,num++)
+			{
+				System.out.print(num);
+			}
+			System.out.println();
+			limit-=2;
+		}
 	}
 }
---
+++ ManojSarathyJava/src/main/java/sarathy/manoj/ManojSarathyJava/array/Creation.java
@@ -0,0 +1,20 @@
+package sarathy.manoj.ManojSarathyJava.array;
+
+public class Creation 
+{
+	public static void main(String[] args) 
+	{
+		int[] statement= {900,10000,300,400,500,100,50,34,1220};
+		
+		System.out.println(statement.length);
+		//var[index]
+		
+		
+		//list
+		for(int pos=0;pos<statement.length;pos++)
+		{
+			System.out.println(statement[pos]);
+		}
+		
+	}
+}
