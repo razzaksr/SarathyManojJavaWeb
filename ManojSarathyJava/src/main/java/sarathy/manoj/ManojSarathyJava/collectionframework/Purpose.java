b7d0639315fc678a2062db89d117d8989e5841a6
razzaksr <razzaksr@gmail.com>
2021-12-03 13:37:12 +0530

    Collections started STACK

---
+++ ManojSarathyJava/src/main/java/sarathy/manoj/ManojSarathyJava/collectionframework/Purpose.java
@@ -0,0 +1,32 @@
+package sarathy.manoj.ManojSarathyJava.collectionframework;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Purpose 
+{
+	public static void main(String[] args) 
+	{
+		Stack hai=new Stack();
+		hai.push("Manoj");hai.push(false);
+		hai.push('R');hai.push(12);hai.push(5.6);
+		hai.push(8765456789L);
+		
+		System.out.println(hai);
+		
+		Iterator it=hai.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+		}
+		
+		System.out.println(hai.empty());
+		
+		System.out.println(hai.pop());
+		hai.pop();
+		
+		System.out.println("Top element: "+hai.peek());
+		
+		System.out.println(hai.search("Manoj"));
+	}
+}
