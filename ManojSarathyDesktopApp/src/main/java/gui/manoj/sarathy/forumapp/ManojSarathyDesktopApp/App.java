8ffc70da79fc9b6abaf5fe83920a68e85b2bcc05
razzaksr <razzaksr@gmail.com>
2021-12-27 11:55:26 +0530

    GUI Initiated

---
+++ ManojSarathyDesktopApp/src/main/java/gui/manoj/sarathy/forumapp/ManojSarathyDesktopApp/App.java
@@ -0,0 +1,13 @@
+package gui.manoj.sarathy.forumapp.ManojSarathyDesktopApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
